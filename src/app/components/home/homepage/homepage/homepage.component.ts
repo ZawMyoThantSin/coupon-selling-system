db33eff6681dae5001bb02d7d2efaab43346677a
Thae Thae <athaemyatnoe.1771@gmail.com>
2024-12-23 15:49:53 +0630

    business image

--- src/app/components/home/homepage/homepage/homepage.component.ts
+++ src/app/components/home/homepage/homepage/homepage.component.ts
@@ -152,7 +152,7 @@ export class HomepageComponent implements OnInit, AfterViewInit {
 
   //Products *** 
   getImageUrl(imagePath: string): any {
-    return this.productService.getImageUrl(imagePath);
+    return this.businessService.getImageUrl(imagePath);
   }
 
   getCouponPrice(productId: number): number {
